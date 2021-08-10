0a5e03eb57b973cdc90b41e112b1c585f6d07c7a
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-08-10 21:04:00 +0100

    put statefull logic in user_transaction widget to prevent main widget like app bar to reload

---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../widgets/transaction_list.dart';
+import '../widgets/new_transaction.dart';
+import '../models/transaction.dart';
+
+class UserTransaction extends StatefulWidget {
+  // UserTransaction({Key? key}) : super(key: key);
+
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: "new Shoes",
+      amount: 70.22,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: "Restaurant",
+      amount: 100.50,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addTranscation(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_userTransactions),
+        NewTransaction(),
+      ],
+    );
+  }
+}
